cfeb7908b81fb0ee69e3cab0c55253473ea0c379
hieu1924 <hieu@specominc.com>
2025-11-24 11:47:47 +0700

    setup admin fe

---
+++ admin-fe/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <>
+      <div>Admin</div>
+      <Button className="cursor-pointer">Click me</Button>
+    </>
+  )
+}
+
+export default App
