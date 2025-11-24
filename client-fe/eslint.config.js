2b77b3a18f02bece7cb009dfa10c1218e6b7f48f
hieu1924 <hieu@specominc.com>
2025-11-24 13:25:12 +0700

    set up client fe

---
+++ client-fe/eslint.config.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import tanstackQuery from '@tanstack/eslint-plugin-query'
+import tanstackRouter from '@tanstack/eslint-plugin-router'
+
+export default defineConfig([
+  globalIgnores(['dist', 'src/routeTrê.gen.ts']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      tanstackQuery.configs['flat/recommended'],
+      tanstackRouter.configs['flat/recommended']
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+])
---
+++ client-fe/src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import './index.css'
+
+// Import the generated route tree
+import { getRouter } from './router'
+
+const router = getRouter()
+
+// Render the app
+const rootElement = document.getElementById('root')
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  )
+}
